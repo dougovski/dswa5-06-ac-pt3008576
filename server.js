9394c61b4bc13d05b4fe981f0145c54f627fbdec
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-11 08:14:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
+const url = 'mongodb://dougovski:1409@clusterdhaps-shard-00-00.rzyac.mongodb.net:27017,clusterdhaps-shard-00-01.rzyac.mongodb.net:27017,clusterdhaps-shard-00-02.rzyac.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-z0ehqd-shard-0&authSource=admin&retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express Server escutando na porta ' + app.get('port'));
 });
